2254a7414f3d6fdaf1ea875006029d242eead527
Ирина <ira.sheina.92@gmail.com>
2023-10-16 15:46:20 +0300

    Добавлено решение Task_003

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -6,5 +6,5 @@ int N = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i <= N; i++)
 {
-    System.Console.WriteLine($"{i} * {i} ^ {i} = {Math.Pow(i, 3)}");
+    System.Console.WriteLine(Math.Pow(i, 3));
 }
